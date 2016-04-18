4f8884beae6da202fd96013d849d9b9fbb8841d7
Max Krieger <themaxaxis@gmail.com>
2016-04-17 23:40:13 -0400

    added 1 stick opmode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/controllers/OneStickDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/controllers/OneStickDrive.java
@@ -1,5 +1,6 @@
 package com.qualcomm.ftcrobotcontroller.controllers;
 
+import com.qualcomm.ftcrobotcontroller.debug.Logger;
 import com.qualcomm.ftcrobotcontroller.neurons.HumanDriving;
 import com.qualcomm.ftcrobotcontroller.organs.TreadDrivetrain;
 import com.qualcomm.ftcrobotcontroller.statics.ControlParser;
@@ -17,7 +18,7 @@ public class OneStickDrive {
     public static void tick() {
         ArrayList<Float> joyValues = ControlParser.range(Controls.JoyDrive);
         ArrayList<Float> powers = HumanDriving.JoyToDirection(joyValues);
-
+        Logger.logLine("left: " + Float.toString(powers.get(0)));
         TreadDrivetrain.drive(powers.get(0), powers.get(1));
     }
 }
