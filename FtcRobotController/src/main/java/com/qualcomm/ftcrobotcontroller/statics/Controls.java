15526acf5429cf7dc455f09d72ad25a3d6ed8d58
maxschool <maxs.school.email@gmail.com>
2016-04-27 09:20:00 -0400

    Update Controls.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/statics/Controls.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/statics/Controls.java
@@ -4,6 +4,6 @@ package com.qualcomm.ftcrobotcontroller.statics;
  * Created by max on 4/17/16.
  */
 public class Controls {
-    public static String Shift = "LT";
+    public static String Shift = "B";
     public static String JoyDrive = "LS1";
 }
