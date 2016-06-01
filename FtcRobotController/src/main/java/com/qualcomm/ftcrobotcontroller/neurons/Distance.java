ff1f98e5bdc447b36e5c17d547fd4723f4f1ec21
maxschool <maxs.school.email@gmail.com>
2016-06-01 18:31:20 -0400

    Update Distance.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/neurons/Distance.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/neurons/Distance.java
@@ -4,9 +4,6 @@ package com.qualcomm.ftcrobotcontroller.neurons;
  * Created by robotics on 5/3/16.
  */
 public class Distance {
-    public static final double WALL_DISTANCE = 13.0;
-
-    @Deprecated
     public static boolean isAtWall(double distance) {
         return (distance < 13.0);
     }
