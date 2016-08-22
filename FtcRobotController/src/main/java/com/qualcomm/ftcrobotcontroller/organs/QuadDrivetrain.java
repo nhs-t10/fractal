208ca74861276044c84696e56d06cbcbd1f3f7a0
Max Krieger <maxkrieger@users.noreply.github.com>
2016-08-22 17:17:49 -0400

    no range.clip

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/organs/QuadDrivetrain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/organs/QuadDrivetrain.java
@@ -32,9 +32,6 @@ public class QuadDrivetrain extends DriveTrain {
     }
 
     public void drive(float left, float right) {
-        right = (float)Range.clip(right, -1.0, 1.0);
-        left = (float)Range.clip(left, -1.0, 1.0);
-
         this.driveQuad(left, left, right, right);
     }
 }
