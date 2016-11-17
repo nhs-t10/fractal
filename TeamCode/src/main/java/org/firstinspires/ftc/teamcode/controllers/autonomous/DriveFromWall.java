7b53b36caabe4f2476eaca7f2a47939a69ebf472
shaashwat <shaash317@gmail.com>
2016-11-17 16:24:30 -0500

    drive AWAY

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/DriveFromWall.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.controllers.autonomous;
+
+import org.firstinspires.ftc.teamcode.controllers.Controller;
+import org.firstinspires.ftc.teamcode.organs.Instruments;
+import org.firstinspires.ftc.teamcode.organs.drivetrains.DriveTrain;
+import org.firstinspires.ftc.teamcode.organs.drivetrains.MecanumDrivetrain;
+
+/**
+ * Created by nhs on 11/17/16.
+ */
+public class DriveFromWall implements Controller {
+    MecanumDrivetrain driveTrain;
+    Instruments instruments;
+    double limit = 0;
+    public DriveFromWall(Instruments i, MecanumDrivetrain dt, double limit) {
+        instruments = i;
+        driveTrain = dt;
+    }
+    public boolean tick() {
+        if (instruments.distance >= limit) {
+            driveTrain.stop();
+            return true;
+        }
+        driveTrain.driveSideways(-0.5f);
+        return false;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/clyde/BeaconAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/clyde/BeaconAuto.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes.clyde;
 
+import org.firstinspires.ftc.teamcode.controllers.Controller;
 import org.firstinspires.ftc.teamcode.controllers.Team;
+import org.firstinspires.ftc.teamcode.controllers.autonomous.DriveFromWall;
 import org.firstinspires.ftc.teamcode.controllers.autonomous.DriveToLine;
+import org.firstinspires.ftc.teamcode.controllers.autonomous.FlickOnce;
 import org.firstinspires.ftc.teamcode.controllers.autonomous.PressBeacon;
 import org.firstinspires.ftc.teamcode.controllers.autonomous.TurnX;
+import org.firstinspires.ftc.teamcode.controllers.tests.Stall;
 import org.firstinspires.ftc.teamcode.opmodes.T10Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.organs.Instruments;
 import org.firstinspires.ftc.teamcode.organs.Pusher;
+import org.firstinspires.ftc.teamcode.organs.Spinner;
 import org.firstinspires.ftc.teamcode.organs.drivetrains.MecanumDrivetrain;
 import org.firstinspires.ftc.teamcode.tissues.TCamera;
 
@@ -25,6 +30,18 @@ public abstract class BeaconAuto extends T10Autonomous {
         Instruments instruments = new Instruments();
         instruments.start();
         Pusher pusher = new Pusher();
+        registerController(new DriveFromWall(instruments, driveTrain, 50.0));
+        registerController(new TurnX(instruments, driveTrain, (team == Team.RED ? -156 : 175)));
+        registerController(new FlickOnce());
+        registerController(new Controller() {
+            @Override
+            public boolean tick() {
+                new Spinner().toggle(1);
+                return true;
+            }
+        });
+        registerController(new Stall(3000));
+        registerController(new FlickOnce());
         registerController(new DriveToLine(instruments, driveTrain, team));
         registerController(new TurnX(instruments, driveTrain, (team == Team.RED ? -90 : 90)));
         registerController(new PressBeacon(team, driveTrain, pusher, camera));
