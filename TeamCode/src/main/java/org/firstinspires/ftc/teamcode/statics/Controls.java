d3abed58109dfaffe1569739dbd8655ca53ac80e
Jacob Gorenburg <jacob_000@Lenovo-PC.(none)>
2016-11-16 21:39:33 -0500

    turning exact amounts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/statics/Controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/statics/Controls.java
@@ -4,6 +4,8 @@ package org.firstinspires.ftc.teamcode.statics;
  * Created by max on 4/17/16.
  */
 public class Controls {
+    public static String Turn90Right = "DR1";
+    public static String Turn90Left = "DL1";
     public static String Shift = "B";
     public static String JoyDrive = "LS1";
     public static String JoySecondary = "RS1";
