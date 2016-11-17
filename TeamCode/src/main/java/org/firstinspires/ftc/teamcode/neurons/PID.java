0ae0db42f561d6284bf41a0d35d871d11ff6abc0
shaashwat <shaash317@gmail.com>
2016-11-17 15:59:05 -0500

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neurons/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neurons/PID.java
@@ -15,7 +15,7 @@ public abstract class PID {
     public double prevError = 1;
     public double integral = 0.0;
     public double getPower (double error) {
-       if ( (.3 > Math.abs(error)) && (.1 > Math.abs((prevError - error)))) {
+       if ( (.5 > Math.abs(error)) && (.2 > Math.abs((prevError - error)))) {
            Logger.logLine("derivative" + (prevError - error));
            integral = Ki * (integral + error);
            prevError = error;
