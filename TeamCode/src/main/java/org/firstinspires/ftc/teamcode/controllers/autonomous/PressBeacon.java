554be91c771f0cf3a2e3be5f5fba9e40e7776dc4
shaashwat <shaash317@gmail.com>
2016-11-18 16:48:29 -0500

    minor fixes - beacon elapsing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/PressBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/PressBeacon.java
@@ -4,6 +4,7 @@ import org.firstinspires.ftc.teamcode.controllers.Controller;
 import org.firstinspires.ftc.teamcode.controllers.Team;
 import org.firstinspires.ftc.teamcode.debug.Logger;
 import org.firstinspires.ftc.teamcode.neurons.BeaconCheck;
+import org.firstinspires.ftc.teamcode.neurons.Time;
 import org.firstinspires.ftc.teamcode.organs.Pusher;
 import org.firstinspires.ftc.teamcode.organs.drivetrains.DriveTrain;
 import org.firstinspires.ftc.teamcode.tissues.TCamera;
@@ -18,16 +19,24 @@ public class PressBeacon implements Controller {
     private TCamera camera;
     private Pusher pusher;
     private BeaconCheck beacon;
+    private Time.Stopwatch sw;
+    private boolean startedCount = false;
     public PressBeacon(Team t, DriveTrain d, Pusher p, TCamera c) {
         driveTrain = d;
         pusher = p;
         camera = c;
+        sw = new Time.Stopwatch();
         beacon = new BeaconCheck(t);
     }
     public boolean tick() {
+        if(!startedCount) {
+            sw.start();
+            startedCount = true;
+        }
         beacon.update(camera.getAnalysis());
         Logger.logLine(camera.getString());
-        if (beacon.isPressed()) {
+        if (beacon.isPressed() && sw.timeElapsed() >= 500) {
+            Logger.logLine("Done pressing.");
             driveTrain.stop();
             return true;
         }
