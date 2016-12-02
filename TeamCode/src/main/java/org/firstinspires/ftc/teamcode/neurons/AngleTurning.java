303cbb0a49d83987e876927e16950332175ff693
Jacob Gorenburg <jacob_000@Lenovo-PC.(none)>
2016-12-02 15:28:30 -0500

    Grave of the Fireflies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neurons/AngleTurning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neurons/AngleTurning.java
@@ -40,7 +40,8 @@ public class AngleTurning extends PID {
         }
         ArrayList<Float> powers = new ArrayList<Float>();
         float power = convertPower(getPower(getError(currentAngle)));
-        if (power < .07 && power != 0) power = .07f;
+        Logger.logLine("OriginalPower: " + power);
+        if (Math.abs(power) < .07 && power != 0) {power = Math.signum(power) * .07f;}
         Logger.logLine("Angle: " + currentAngle + 180);
         Logger.logLine("Error: "+ getError(currentAngle));
         Logger.logLine("Power: " + power);
