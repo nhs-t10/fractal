5d10a603a4c39ff296ddd9d716530342cc476fe9
Max Krieger <themaxaxis@gmail.com>
2016-12-08 18:48:35 -0500

    remapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/statics/Controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/statics/Controls.java
@@ -4,21 +4,25 @@ package org.firstinspires.ftc.teamcode.statics;
  * Created by max on 4/17/16.
  */
 public class Controls {
+    // SYSTEM
     public static final String Shift = "B";
     public static final String Terminate = "BK1";
+    public static final String Selector = "LS1";
+    // DRIVING
     public static final String AutoPressRight = "DR1";
     public static final String AutoPressLeft = "DL1";
-    public static final String AutoAlign = "X1";
+    public static final String AutoAlign = "LSB1";
     public static final String JoyDrive = "LS1";
     public static final String JoySecondary = "RS1";
-    public static final String Flicker = "LT1";
-    public static final String Stopper = "DD1";
-    public static final String SpinnerIn = "Y1";
-    public static final String SpinnerOut = "^Y1";
-    public static final String BallLift = "DU1";
     public static final String LeftPush = "LB1";
     public static final String RightPush = "RB1";
-    public static final String Selector = "LS1";
+    // SCORING
+    public static final String Flicker = "LT*";
+    public static final String Stopper = "RT2";
+    public static final String SpinnerIn = "LB2";
+    public static final String BallLift = "RB2";
+    public static final String SpinnerOut = "X2";
+    // OLD
     @Deprecated
     public static final String DriveRight = "A1";
     @Deprecated
