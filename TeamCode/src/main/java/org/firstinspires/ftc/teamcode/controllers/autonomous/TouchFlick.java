0bdcd173ab7cbc87170f1a7dfe0e4cf63b820356
William <pyroninja88211@gmail.com>
2016-12-16 14:57:36 -0500

    added timeout parameter for touchflick. Untested! if it breaks shit, revert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/TouchFlick.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/TouchFlick.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.controllers.autonomous;
 
 import org.firstinspires.ftc.teamcode.controllers.Controller;
 import org.firstinspires.ftc.teamcode.controllers.tests.Stall;
+import org.firstinspires.ftc.teamcode.debug.Logger;
 import org.firstinspires.ftc.teamcode.neurons.Time;
 import org.firstinspires.ftc.teamcode.organs.Flicker;
 import org.firstinspires.ftc.teamcode.statics.Hardware;
@@ -14,22 +15,37 @@ public class TouchFlick implements Controller {
     private Flicker flicker;
     private TTouch touch = new TTouch(Hardware.Touch);
     private Time.Stopwatch sw = new Time.Stopwatch();
+    private Time.Stopwatch timeoutTimer = new Time.Stopwatch();
 
     private boolean initialHit = false;
     private int delay;
+    private int timeout;
 
     public TouchFlick(Flicker fl) {
-        this(fl, -1);
+        this(fl, -1, -1);
     }
 
     public TouchFlick(Flicker fl, int d) {
+        this(fl, d, -1);
+    }
+
+    public TouchFlick(Flicker fl, int d, int t) {
         flicker = fl;
         delay = d;
+        timeout = t;
     }
 
     @Override
     public boolean tick() {
-
+        //kill macro if takes to long
+        //off by default
+        if(!timeoutTimer.isRecording()) {
+            timeoutTimer.start();
+        } else if(timeout >= 0 && timeoutTimer.timeElapsed() > timeout) {
+            Logger.logLine("Macro ran out of time!");
+            deinit();
+            return true;
+        }
 
         if(touch.isPressed()) {
             flicker.engage();
@@ -42,13 +58,18 @@ public class TouchFlick implements Controller {
         if(initialHit && !sw.isRecording()) {
             sw.start();
         } else if(initialHit && sw.timeElapsed() > delay) {
-            flicker.stop();
-            flicker.lock(false);
-            sw.stop();
-            initialHit = false;
+            deinit();
             return true;
         }
 
         return false;
+    }
+
+    private void deinit() {
+        flicker.stop();
+        flicker.lock(false);
+        sw.stop();
+        timeoutTimer.stop();
+        initialHit = false;
     }
 }
