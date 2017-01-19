53813d987a3f045a84faa69383562e801110afe7
bedheadjack <bedheadjack@gmail.com>
2017-01-19 15:19:56 -0500

    linearize fn

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neurons/LinearizedDistance.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.neurons;
+
+/**
+ * Created by Jack on 1/19/2017.
+ */
+
+public class LinearizedDistance {
+    public double linearize(double d) {
+        if (d == 3) d = 2.99;
+        return (double) 6787/(d - 3) - 4;
+    }
+}
