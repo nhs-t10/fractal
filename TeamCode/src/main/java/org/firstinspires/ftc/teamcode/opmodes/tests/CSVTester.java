8f914c470dded30b8b8788da17ec90b5a049a7ef
William <pyroninja88211@gmail.com>
2017-01-31 13:36:49 -0500

    we now have write permissions!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/CSVTester.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+/**
+ * Created by robotics on 1/31/17.
+ */
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.debug.DataPlot;
+import org.firstinspires.ftc.teamcode.opmodes.T10Opmode;
+
+@TeleOp(name="CSV", group="Testers")
+public class CSVTester extends T10Opmode {
+    DataPlot dp;
+
+    @Override
+    public void run() {
+        dp = new DataPlot();
+    }
+
+    @Override
+    public void tick() {
+        dp.logData(1, 1);
+    }
+}
