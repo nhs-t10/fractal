edbd3b3f930b05014bd60b48c18f04b7c255dd6d
Jacob <Enemiesgate@gmail.com>
2017-02-02 15:59:48 -0500

    logging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/TurnX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/autonomous/TurnX.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.controllers.autonomous;
 
 import org.firstinspires.ftc.teamcode.controllers.Controller;
+import org.firstinspires.ftc.teamcode.debug.Logger;
 import org.firstinspires.ftc.teamcode.neurons.AngleTurning;
+import org.firstinspires.ftc.teamcode.neurons.Time;
 import org.firstinspires.ftc.teamcode.organs.Instruments;
 import org.firstinspires.ftc.teamcode.organs.drivetrains.DriveTrain;
 
@@ -15,14 +17,20 @@ public class TurnX implements Controller {
     private DriveTrain driveTrain;
     private Instruments instruments;
     private AngleTurning angleTurning;
-    public int test;
+    public boolean started = false;
+    public Time.Stopwatch sw = new Time.Stopwatch();
     public TurnX(Instruments i, DriveTrain d, double deg) {
         instruments = i;
         driveTrain = d;
         angleTurning = new AngleTurning(deg);
     }
     public boolean tick (){
+        if(!started) {
+            sw.start();
+            started = true;
+        }
         ArrayList<Float> values = angleTurning.getPivotPowers(instruments.yaw);
+        Logger.logFile("yaw", instruments.yaw + "," + sw.timeElapsed());
         if (values.get(0) == 0.0 && values.get(1) == 0.0) {
             driveTrain.stop();
             return true;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/AutonomousTest.java
@@ -1,15 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.controllers.Controller;
 import org.firstinspires.ftc.teamcode.controllers.Team;
 import org.firstinspires.ftc.teamcode.controllers.autonomous.AverageTime;
 import org.firstinspires.ftc.teamcode.controllers.autonomous.DriftToLine;
+import org.firstinspires.ftc.teamcode.controllers.autonomous.TurnX;
 import org.firstinspires.ftc.teamcode.opmodes.T10Linear;
 import org.firstinspires.ftc.teamcode.organs.Instruments;
 import org.firstinspires.ftc.teamcode.organs.Pusher;
 import org.firstinspires.ftc.teamcode.organs.drivetrains.MecanumDrivetrain;
 import org.firstinspires.ftc.teamcode.tissues.TCamera;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 
 /**
  * Created by Admin on 4/19/2016.
@@ -24,7 +39,14 @@ public class AutonomousTest extends T10Linear {
         Instruments instruments = new Instruments();
         instruments.start();
         cam = new TCamera();
-        registerController(new AverageTime());
+//        registerController(new AverageTime());
+        registerController(new TurnX(instruments, md, 90));
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.GREEN);
+            }
+        });
 //        registerController(new DriftToLine(instruments, md, -0.1f, true));
 //        registerController(new PressBeacon(true, Team.RED, instruments, md, p, cam));
 //        registerController(new TurnX(instruments, md, 0));
