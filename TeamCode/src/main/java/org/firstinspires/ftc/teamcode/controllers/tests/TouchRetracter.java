e2f3c26290634f04069d4631b605b447a41bd543
Max Krieger <themaxaxis@gmail.com>
2017-02-18 10:48:00 -0500

    maintenance opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/tests/TouchRetracter.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.controllers.tests;
+
+import org.firstinspires.ftc.teamcode.controllers.Controller;
+import org.firstinspires.ftc.teamcode.organs.ButtonRoller;
+import org.firstinspires.ftc.teamcode.organs.RollerStopper;
+import org.firstinspires.ftc.teamcode.organs.Spacers;
+
+/**
+ * Created by max on 2/18/17.
+ */
+public class TouchRetracter implements Controller {
+    private RollerStopper rollerStopper;
+    private ButtonRoller buttonRoller;
+    private Spacers spacers;
+
+    private boolean debounce = false;
+    public TouchRetracter(RollerStopper rollerStopper, ButtonRoller buttonRoller, Spacers spacers) {
+        this.rollerStopper = rollerStopper;
+        this.buttonRoller = buttonRoller;
+        this.spacers = spacers;
+    }
+    public boolean tick() {
+        if(spacers.isTouchingRight()) spacers.lower();
+        else spacers.stop();
+
+        if(buttonRoller.isTouching()) buttonRoller.wind();
+        else buttonRoller.stop();
+
+        if(!debounce && spacers.isTouchingLeft()) {
+            debounce = true;
+            rollerStopper.toggle();
+        }
+        else if(!spacers.isTouchingLeft()) {
+            debounce = false;
+        }
+        return false;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/bonnie/Maintenance.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes.bonnie;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.controllers.Controller;
+import org.firstinspires.ftc.teamcode.controllers.tests.TouchRetracter;
+import org.firstinspires.ftc.teamcode.opmodes.T10Opmode;
+import org.firstinspires.ftc.teamcode.organs.ButtonRoller;
+import org.firstinspires.ftc.teamcode.organs.RollerStopper;
+import org.firstinspires.ftc.teamcode.organs.Spacers;
+
+import java.util.ArrayList;
+
+/**
+ * Created by max on 2/18/17.
+ */
+@TeleOp(name="Maintenance", group="Bonnie")
+public class Maintenance extends T10Opmode {
+    private ArrayList<Controller> controllers = new ArrayList<Controller>();
+    @Override
+    public void run() {
+        controllers.add(new TouchRetracter(new RollerStopper(), new ButtonRoller(), new Spacers()));
+    }
+    public void tick() {
+        for(int i=0; i<controllers.size(); i++) {
+            controllers.get(i).tick();
+        }
+    }
+}
